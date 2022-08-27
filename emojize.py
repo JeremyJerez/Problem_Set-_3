818b2d005f4469e79882af8688cb22b3a54f2d20
Jeremy Jerez <110129245+JeremyJerez@users.noreply.github.com>
2022-08-27 18:51:29 -0500

    Create emojize.py
    I do not support plagiarism, this it's an example of my basic approach to the problem
    based on the course teachings.
    
    remember to install the library, on terminal window type:
    pip install emoji

---
+++ emojize.py
@@ -0,0 +1,12 @@
+# Python_Intro
+# Problem Set 4
+#A series of exercises for CS50 hands-on projects
+"""
+This one's my approach to the "Emojize" problem
+"""
+import emoji
+
+text = input("input: ")
+
+emojized = emoji.emojize(text)
+print("output:", emojized)
